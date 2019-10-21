d7b8e9bc54fa15b70a7cfdf85230842d3dc93f1c
erverted <ervinmitra@gmail.com>
2019-10-21 16:43:55 -0500

    reassigned

--- PointersConsoleApp.cpp
+++ PointersConsoleApp.cpp
@@ -12,6 +12,10 @@ void printVals()
 	std::cout << "val: " << val << "\nval2: " << val2 << "\nvalPtr: " << *valPtr << "\nvalRef: " << valRef << std::endl;
 	std::cout << "Addresses:" << std::endl;
 	std::cout << "val: " << &val << "\nval2: " << &val2 << "\nvalPtr: " << valPtr << "\nvalRef: " << &valRef << std::endl;
+
+	// reassign pointer and reference (2nd assignment)
+	valPtr = &val2;
+	valRef = val2;
 }
 
 int main()
